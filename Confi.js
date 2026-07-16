7b3b877cf008bd1cb3019ecde3b29ad2c4c551ef
ap9641157-boop <ap9641157@gmail.com>
2026-07-16 15:51:02 -0400

    Add configuration file Confi.js

---
+++ Confi.js
@@ -0,0 +1,8 @@
+[
+  {
+    "enabled": true,
+    "name": "Mi ffxit v3",
+    "url": "*GetWeaponData*,*WeaponConfig*,*BallisticData*,*HitboxData*",
+    "remoteUrl": "https://raw.githubusercontent.com/TU_USUARIO/mis-codigos-ff/main/proxy.js"
+  }
+]
